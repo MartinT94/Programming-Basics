3e207332e2b22dbab4264de91505c9ea6b81ee15
Martin <m.todorov94@gmail.com>
2017-02-28 18:14:51 +0200

    StringExercise

---
+++ Strings/Palindromes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(new[] { ' ', ',', '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var palindromes = new List<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == Reversed(input[i]))
+                {
+                    palindromes.Add(input[i]);
+                }
+            }
+            
+
+            Console.Write(string.Join(", ", palindromes.OrderBy(x => x).Distinct()));
+
+        }
+
+        public static string Reversed(string input)
+        {
+            var lettRev = input.ToCharArray();
+            Array.Reverse(lettRev);
+            return new string(lettRev);
+        }
+    }
+}
