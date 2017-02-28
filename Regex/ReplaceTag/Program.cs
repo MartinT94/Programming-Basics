caac2484acc0f2b222fc3e2670789cfeb7b67f74
Martin <m.todorov94@gmail.com>
2017-02-28 18:11:44 +0200

    Regex

---
+++ Regex/ReplaceTag/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReplaceTag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+
+            while (text != "end")
+            {
+                var pattern = @"<a.*?href.*?=(.*)>(.*?)<\/a>";
+
+                var replace = @"[URL href=$1]$2[/URL]";
+
+                var replaced = Regex.Replace(text, pattern, replace);
+
+                Console.WriteLine(replaced);
+
+                text = Console.ReadLine();
+            }
+            
+        }
+    }
+}
