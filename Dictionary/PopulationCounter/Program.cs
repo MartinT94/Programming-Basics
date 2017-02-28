353d28179ab49d214a56808d5bd90609a375e0b1
Martin <m.todorov94@gmail.com>
2017-02-28 18:10:50 +0200

    Dictionary

---
+++ Dictionary/PopulationCounter/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopulationCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+
+            var countrys = new Dictionary<string, Dictionary<string, long>>();
+            while (line != "report")
+            {
+                var data = line.Split('|');
+                
+                var citys = data[0];
+                var country = data[1];
+                var population = int.Parse(data[2]);
+
+                if (!countrys.ContainsKey(country))
+                {
+                    countrys[country] = new Dictionary<string, long>();
+                }
+                if (countrys.ContainsKey(country))
+                {
+                    countrys[country].Add(citys, population);
+                }
+                
+                line = Console.ReadLine();
+
+            }
+
+            
+            foreach (var city in countrys.OrderByDescending(x => x.Value.Values.Sum()))
+            {
+                var sum = city.Value.Values.Sum();
+                
+                Console.WriteLine($"{city.Key} (total population: {sum})");
+
+                foreach (var item in city.Value.OrderByDescending(k => k.Value))
+                {
+                    Console.WriteLine($"=>{item.Key}: {item.Value}");
+                }
+            }
+        }
+    }
+}
