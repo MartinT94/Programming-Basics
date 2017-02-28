ec4c6a76445f802fbde296d02fdb8914447b81f7
Martin <m.todorov94@gmail.com>
2017-02-28 18:11:35 +0200

    Regex

---
+++ Regex/ExtractSentencesByKeyWord/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExtractSentencesByKeyWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine();
+
+            var input = Console.ReadLine();
+
+            var pattern = @"([^.!?]+)([\W])";
+
+            var pattern1 = string.Format("\\b" + word + "\\b");
+            
+            var regex = new Regex(pattern);
+
+            var keyWord = new Regex(pattern1);
+
+            var matches = regex.Matches(input);
+
+            
+
+            foreach (Match match in matches)
+            {
+                if (keyWord.IsMatch(match.Groups[1].Value))
+                {
+                    Console.WriteLine(match.Groups[1].Value.Trim());
+                }
+               
+            }
+        }
+    }
+}
