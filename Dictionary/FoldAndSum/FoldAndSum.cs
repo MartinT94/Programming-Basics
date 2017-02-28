25712991f2b6bf44dfdef2b254bff95761f0e7d7
Martin <m.todorov94@gmail.com>
2017-02-28 18:09:18 +0200

    Dictionary

---
+++ Dictionary/FoldAndSum/FoldAndSum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoldAndSum
+{
+    public class FoldAndSum
+    {
+         public static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int k = input.Length / 4;
+
+            var leftRow =
+                input.Take(k)
+                .Reverse()
+                .ToArray();
+
+            var rightRow =
+                input
+                .Reverse()
+                .Take(k)
+                .ToArray();
+
+            var firstRow = leftRow
+                .Concat(rightRow)
+                .ToArray();
+
+            var middle = input
+                .Skip(k)
+                .Take(k * 2)
+                .ToArray();
+
+            var sum = firstRow.Select((x, index) => x + middle[index]);
+
+            Console.WriteLine(string.Join(" ", sum));
+
+        }
+    }
+}
