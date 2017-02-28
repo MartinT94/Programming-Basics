94ba0a2acdb72a382ad185efde9c68309ae66d5f
Martin <m.todorov94@gmail.com>
2017-02-28 17:59:57 +0200

    Strings

---
+++ Strings/TextFilter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bannedWords = Console.ReadLine().Split(new[] { ',' , ' '}, StringSplitOptions.RemoveEmptyEntries);
+            var text = Console.ReadLine();
+
+            var sb = new StringBuilder(text);
+
+            foreach (var word in bannedWords)
+            {
+                sb.Replace(word, new string('*', word.Length));
+            }
+
+            Console.WriteLine(sb);
+            
+        }
+    }
+}
