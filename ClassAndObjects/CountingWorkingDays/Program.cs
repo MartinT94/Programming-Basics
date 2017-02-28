f5504ec5ef8ee64b14ab6d3a47c0ce7fd3d2b1d3
Martin <m.todorov94@gmail.com>
2017-02-28 18:06:35 +0200

    ClassAndObjects

---
+++ ClassAndObjects/CountingWorkingDays/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CountingWorkingDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstDate = Console.ReadLine();
+            var secondDate = Console.ReadLine();
+
+            var startDate = DateTime.ParseExact(firstDate, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(secondDate, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            DateTime[] holidays = new DateTime[12];
+
+            holidays[0] = new DateTime(4, 01, 01);
+            holidays[1] = new DateTime(4, 03, 03);
+            holidays[2] = new DateTime(4, 05, 01);
+            holidays[3] = new DateTime(4, 05, 06);
+            holidays[4] = new DateTime(4, 05, 24);
+            holidays[5] = new DateTime(4, 09, 06);
+            holidays[6] = new DateTime(4, 09, 22);
+            holidays[7] = new DateTime(4, 11, 01);
+            holidays[9] = new DateTime(4, 12, 24);
+            holidays[10] = new DateTime(4, 12, 25);
+            holidays[11] = new DateTime(4, 12, 26);
+
+            int workingDays = 0;
+
+            for (var date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                DateTime temp = new DateTime(4 ,date.Month,date.Day);
+
+                if (!holidays.Contains(temp)  && date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    workingDays++;
+                }
+            }
+
+            Console.WriteLine(workingDays);
+        }
+    }
+}
