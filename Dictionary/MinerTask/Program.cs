a08ace6d3b2b96c047a7a93ecea4695e3d4b85fe
Martin <m.todorov94@gmail.com>
2017-02-28 18:10:20 +0200

    Dictionary

---
+++ Dictionary/MinerTask/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool runing = false;
+
+            List<string> minerList = new List<string>();
+
+            var dict = new Dictionary<string, int>();
+
+            while (!runing)
+            {
+                string input = Console.ReadLine();
+                minerList.Add(input);
+
+                if (input == "stop")
+                {
+                    runing = true;
+                }
+                
+            }
+            for (int i = 0; i < minerList.Count - 1; i+= 2)
+            {
+                if (!dict.ContainsKey(minerList[i]))
+                {
+                    dict.Add(minerList[i], int.Parse(minerList[i + 1]));
+                }
+                else if (dict.ContainsKey(minerList[i]))
+                {
+                    dict[minerList[i]] += int.Parse(minerList[i + 1]);
+                }
+                
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
