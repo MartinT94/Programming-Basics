55abe45e98ec4c08765661cc565ccdce37ee7108
Martin <m.todorov94@gmail.com>
2017-02-28 18:14:37 +0200

    StringExercise

---
+++ StringExercise/UniCodeCharacters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniCodeCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToCharArray();
+
+            foreach (var letter in input)
+            {
+                Console.Write(GetEscapeSequence(letter));
+            }
+            
+        }
+
+        public static string GetEscapeSequence(char c)
+        {
+            return "\\u" + ((int)c).ToString("x4");
+        }
+    }
+}
